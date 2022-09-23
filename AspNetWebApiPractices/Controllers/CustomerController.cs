3801606e54ecb30cc5d73d143d2f49704808fdaa
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-09-23 09:48:10 +0330

    Added customer controller

---
+++ AspNetWebApiPractices/Controllers/CustomerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiPractices.Controllers
+{
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
