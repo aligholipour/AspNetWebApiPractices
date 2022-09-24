d6a5b761c0a9b3385eb10919f3ab9bfe93f80f71
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-09-24 11:17:52 +0330

    Creating problem details when our state model is not valid and returning an Status 422 Unprocessable Entity

--- AspNetWebApiPractices/Program.cs
+++ AspNetWebApiPractices/Program.cs
@@ -1,5 +1,8 @@
 using AspNetWebApiPractices.Infrastructures;
 using AspNetWebApiPractices.Services.Customers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +13,36 @@ builder.Services.AddControllers(setup =>
 {
     setup.ReturnHttpNotAcceptable = true;
 
-}).AddXmlDataContractSerializerFormatters();
+}).AddXmlDataContractSerializerFormatters().ConfigureApiBehaviorOptions(setup =>
+{
+    setup.InvalidModelStateResponseFactory = context =>
+    {
+        var problemDetailsFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+        var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState);
+
+        problemDetails.Instance = context.HttpContext.Request.Path;
+
+        var actionExecutedContext = context as ActionExecutingContext;
+
+        if (context.ModelState.ErrorCount > 0 && (actionExecutedContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+        {
+            problemDetails.Type = "https://courselibrary.com/modelvalidationproblem";
+            problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+            problemDetails.Title = "One or more validation errors occurred.";
+            problemDetails.Extensions.Add("traceId", context.HttpContext.TraceIdentifier);
+
+            return new UnprocessableEntityObjectResult(problemDetails)
+            {
+                ContentTypes = { "application/problem+json" }
+            };
+        }
+
+        return new BadRequestObjectResult(problemDetails)
+        {
+            ContentTypes = { "application/problem+json" }
+        };
+    };
+});
 
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
