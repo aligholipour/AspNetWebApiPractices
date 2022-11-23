a6cbb7ddda63b951398791e354f1f29ac44c91bc
Ali Gholipour <aligholipourkhalili@gmail.com>
2022-11-23 18:22:28 +0330

    Customer controller version 2 added

---
+++ AspNetWebApiPractices/Controllers/CustomerControllerV2.cs
@@ -0,0 +1,24 @@
+﻿using AspNetWebApiPractices.Models.Customers;
+using AspNetWebApiPractices.Services.Customers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetWebApiPractices.Controllers
+{
+    [ApiController]
+    [Route("api/v{version:apiVersion}/customers")]
+    [ApiVersion("2.0")]
+    public class CustomerControllerV2 : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCustomers()
+        {
+            var customers = new CustomerDto
+            {
+                Id= 1,
+                FullName = "test fullname",
+            };
+
+            return Ok(customers);
+        }
+    }
+}
